2553be72b40c9d1c69c9396b75dcb8923e2c40c0
BirdyydriB <loizeauanthony@gmail.com>
2020-04-13 11:39:35 +0200

    default bg for sortFunction

--- src/templates/sortFunction.js
+++ src/templates/sortFunction.js
@@ -1,4 +1,4 @@
 module.exports = `
-<div id="<%= sort_id %>" class="sortFunction <%= sort_class %> mx-1 py-1 px-4 rounded-full cursor-pointer select-none">
+<div id="<%= sort_id %>" class="sortFunction <%= sort_class %> mx-1 py-1 px-4 rounded-full cursor-pointer select-none bg-gray-600">
   <%= sort_name %>
 </div>`;
